5fe93ecb0a76c9df7ff0649bcbafdc7c2ea75c3b
lenyoun <132416519+lenyoun@users.noreply.github.com>
2023-05-04 12:04:20 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -20,8 +20,8 @@ def lasso(data):
     x = np.insert(x, 0, 1, axis=1)
     lam = np.exp(-12)
     w = np.zeros(x.shape[1])
-    alpha = 1000000
-    for i in range(10000):
+    alpha = 0.01
+    for i in range(15000):
         w[1:] -= alpha * (x[:, 1:].T.dot(x[:, 1:].dot(w[1:]) - y) + lam * np.sign(w[1:]))
         w[0] -= alpha * (x[:, 0].T.dot(x[:, 0].dot(w[0]) - y))
     return np.dot(data, w[1:]) + w[0]
